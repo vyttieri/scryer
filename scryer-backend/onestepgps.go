98991e418535b3aa23dabb59bd7321a83c2c8184
deakolt <alexandervaccaro@gmail.com>
2022-07-26 11:50:03 -0700

    Add endpoint for OneStepGPS API data, use Go Cacheing

--- scryer-backend/onestepgps.go
+++ scryer-backend/onestepgps.go
@@ -10,7 +10,7 @@ import (
 var apiKey = os.Getenv("ONESTEPGPS_API_KEY")
 var url = fmt.Sprintf("https://track.onestepgps.com/v3/api/public/device?latest_point=true&api-key=%s", apiKey)
 
-func Get() []byte {
+func GetDeviceData() []byte {
 	fmt.Println("starting onestepgps GET")
 	fmt.Printf("getting URL %s", url)
 	response, error := http.Get(url)
@@ -30,7 +30,6 @@ func Get() []byte {
 	return body
 }
 
-func main() {
-	Get()
-	// fmt.Println(body)
-}
+// func main() {
+// 	GetDeviceData()
+// }
