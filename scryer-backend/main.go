bbf59307f5d07cd1af626bbbf980622db54ac6d9
deakolt <alexandervaccaro@gmail.com>
2022-07-27 11:06:05 -0700

    main takes a commandline argument

--- scryer-backend/main.go
+++ scryer-backend/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
+
 	"scryer-backend/db"
 	"scryer-backend/server"
 )
 
 func main() {
-	switch "" {
+	flag.Parse()
+	cmd := flag.Arg(0)
+	switch cmd {
 	case "migrate":
 		db.RunMigrations()
 	case "server":
