53152fc712d1231b5915ed168d214806b0c29bde
deakolt <alexandervaccaro@gmail.com>
2022-08-01 15:43:01 -0700

    Add some client-side form validations

--- scryer-frontend/src/components/LoginForm.vue
+++ scryer-frontend/src/components/LoginForm.vue
@@ -1,12 +1,25 @@
 <script setup>
-import { useAuthStore } from '../stores/auth.js'
-const authStore = useAuthStore()
+import { useAuthStore } from '@/stores/auth'
+const { login } = useAuthStore()
 </script>
 
 <template>
-  <q-form @submit="authStore.login(username, password)">
-    <q-input v-model="username" filled type="text" hint="Username" />
-    <q-input v-model="password" filled type="password" hint="Password" style="margin-bottom: 10px;" />
+  <q-form @submit="login(username, password)">
+    <q-input
+      v-model="username"
+      filled
+      type="text"
+      hint="Username"
+      :rules="[ val => val.length > 0 || 'Please enter a username' ]"
+    />
+    <q-input
+      v-model="password"
+      filled
+      type="password"
+      hint="Password"
+      style="margin-bottom: 10px;"
+      :rules="[ val => val.length > 0 || 'Please enter a password' ]"
+    />
     <q-btn label="Login" type="submit" />
     <a @click="$emit('toggle-forms')" class="cursor-pointer" style="padding: 5px; margin-left: 20px; display: inline-block;">Register</a>
     <div v-if="errors">{{errors.ApiError}}</div>
--- scryer-frontend/src/components/RegistrationForm.vue
+++ scryer-frontend/src/components/RegistrationForm.vue
@@ -1,17 +1,50 @@
 <script setup>
 import { useUserStore } from '@/stores/user'
-
-const userStore = useUserStore()
+const { register } = useUserStore()
 </script>
 
 <template>
-  <q-form @submit="userStore.register(username, password, passwordConfirmation)">
-      <q-input v-model="username" filled type="text" hint="Username" />
-      <q-input v-model="password" filled type="password" hint="Password" />
-      <q-input v-model="passwordConfirmation" filled type="password" hint="Confirm Password" style="margin-bottom: 10px;" />
-      <q-btn label="Register" type="submit" />
-      <a @click="$emit('toggle-forms')" class="cursor-pointer" style="padding: 5px; margin-left: 20px; display: inline-block;">Login</a>
-    <div v-if="errors">{{errors.ApiError}}</div>
+  <q-form @submit="register(username, password, passwordConfirmation)">
+    <q-input
+      v-model="username"
+      filled
+      type="text"
+      hint="Username"
+      :rules="[ val => val.length <= 24 || 'Please use a maximum of 32 characters',
+                val => val.length > 0 || 'Please enter a username' ]"
+    />
+    <q-input
+      v-model="password"
+      filled
+      type="password"
+      hint="Password"
+      :rules="[ val => val.length <= 32 || 'Please use a maximum of 64 characters',
+                val => val.length > 0 || 'Please enter a password']"
+    />
+    <q-input
+      v-model="passwordConfirmation"
+      filled
+      type="password"
+      hint="Confirm Password"
+      style="margin-bottom: 10px;"
+      :rules="[ val => val.length <= 32 || 'Please use a maximum of 64 characters',
+          val => val.length > 0 || 'Please confirm password']"
+ />
+    <q-btn label="Register" type="submit" />
+    <a @click="$emit('toggle-forms')" class="cursor-pointer" style="padding: 5px; margin-left: 20px; display: inline-block;">Login</a>
+    <!-- <div v-if="errors">{{errors}}</div> -->
   </q-form>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        errors: null,
+        username: '',
+        password: '',
+        passwordConfirmation: ''
+      }
+    }
+  }
+</script>
